966ef5d571580f1793f1e23e9793edde3c944e52
printf-Shivam <shivamkrsingh264@gmail.com>
2026-02-16 06:26:30 +0000

    simple multithreaded crawler

--- web_crawler/src/CrawlerEngine.java
+++ web_crawler/src/CrawlerEngine.java
@@ -1,5 +1,9 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -8,65 +12,60 @@ import org.jsoup.nodes.Element;
 
 public class CrawlerEngine implements Runnable{
 
-	private static final int depth =3;
-	private Thread thread;
+	private static final int MAX_THREAD =4;
+    private static final int MAX_DEPTH =4;
 	private String first_url;
-	private ArrayList<String> visited = new ArrayList<>();
-	private int ID;
-	
-	public CrawlerEngine(String url, int num) {
-		System.out.println("Web Crawler created");
-		first_url = url;
-		ID= num;
-		
-		thread = new Thread(this);
-		thread.start();
+	private int depth;
+
+	private static ExecutorService executor = Executors.newFixedThreadPool(MAX_THREAD);
+	private static Set<String> visited = ConcurrentHashMap.newKeySet();
+
+	public CrawlerEngine(String link, int d) {
+		first_url = link;
+		depth= d;
 	}
-	
 	
 	@Override
 	public void run() {
-		crawl(1,first_url);
+		crawl(first_url,depth);
+	}
+	public void start(){
+		executor.submit(new CrawlerEngine(first_url,1));
 	}
 	
-	private void crawl ( int level, String url) {
-		if(level<=depth) {
-			Document doc = request(url, visited);
-			
-			if(doc!=null) {
-				for( Element link : doc.select("a[href]")) {
-					String next_link = link.absUrl("href");
-					if(visited.contains(next_link)== false) {
-						crawl(level++,next_link);
-					}
+	private void crawl (String url, int depth) {
+		if(depth>MAX_DEPTH) return;
+
+		if(!visited.add(url)) return;
+
+		Document doc = request(url);
+					
+		if(doc!=null) {
+			for( Element link : doc.select("a[href]")) {
+				String next_link = link.absUrl("href");
+				if(!visited.contains(next_link)) {
+					executor.submit(new CrawlerEngine(next_link, depth+1));
 				}
 			}
 		}
 	}
 	
-	private Document request(String url, ArrayList<String> list) {
+	private Document request(String url) {
 		try {
-			Connection con = Jsoup.connect(url);
+			Connection con = Jsoup.connect(url).timeout(5000);
 			Document doc = con.get();
 			
 			if(con.response().statusCode()==200) {
-				System.out.println("\n ** Bot ID: " + ID + ", Received Webpage at: "+ url);
-				
-				String title = doc.title();
-				System.out.println(title);
-				list.add(url);
+				System.out.println(Thread.currentThread().getName()+" visited: "+url);
 			}
 			return doc;
 		}
 		
 		catch(IOException e) {
+			System.err.println("error in request method");
 			return null;
 		}
 		
-	}
-	
-	public Thread getthreads() {
-		return thread;
 	}
 	
 }
